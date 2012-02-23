6fcbd4826228ffa554a87e9aa74429d3b787fb78
berdsen.home <berdsen.home@gmail.com>
2012-02-23 18:21:28 +0000

    - added Firmware Driver

--- Arduino-Board-Firmware/Firmware_BA_Robot/Firmware_BA_Robot.ino
+++ Arduino-Board-Firmware/Firmware_BA_Robot/Firmware_BA_Robot.ino
@@ -1,15 +1,46 @@
 
 #include "ServoMotor.h"
+#include <UsbDevice.h>
 
 ServoMotor servo[8];
+
 // Initializing Routine
 void setup() {      
-  for (int i = 0; i < 8; i++)
-  {  
-    servo[i] = ServoMotor();
-  }
+  Serial.begin(9600);
+  inputString.reserve(200);
 }
 
 void loop() {
-
+  
+  
+  if (stringComplete) 
+  {
+    if (inputString == "")
+    {
+      stringComplete = false;      
+    }
+    else
+    {
+      Serial.println(inputString); 
+      // clear the string:
+      inputString = "";
+      stringComplete = false;
+    }
+  }
 }
+
+void serialEvent() 
+{
+  while (Serial.available()) 
+  {
+    char inChar = (char)Serial.read(); 
+    if (inChar == '\n' || inChar == '\r') {
+      stringComplete = true;
+    }
+    else
+    { 
+      inputString += inChar;
+    } 
+  }
+}
+
