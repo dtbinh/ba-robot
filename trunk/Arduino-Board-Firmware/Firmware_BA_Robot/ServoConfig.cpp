8266f901c718563b8478139014a16ae5e1425764
berdsen.home@gmail.com <berdsen.home@gmail.com@e0f09661-beb8-84b9-8efb-b0379848397b>
2012-02-23 12:01:57 +0000

    - started Firmware Programming for Arduino Mega ADK

---
+++ trunk/Arduino-Board-Firmware/Firmware_BA_Robot/ServoConfig.cpp
@@ -0,0 +1,8 @@
+#include "ServoConfig.h"
+
+ServoConfig::ServoConfig()
+{
+  this->valueMax = 0;
+  this->valueMin = 0;
+}
+
---
+++ trunk/Arduino-Board-Firmware/Firmware_BA_Robot/ServoConfig.h
@@ -0,0 +1,14 @@
+#ifndef SERVOCONFIG_H_
+#define SERVOCONFIG_H_
+
+class ServoConfig
+{
+private:
+  int valueMax;
+  int valueMin;
+  
+public:
+  ServoConfig();
+};
+
+#endif // SERVOCONFIG_H_
