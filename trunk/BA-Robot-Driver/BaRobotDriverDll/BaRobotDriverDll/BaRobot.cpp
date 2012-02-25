280841d6336f56bf9fa09b367a2ad6fe70d35de8
berdsen.home@gmx.de <berdsen.home@gmx.de@e0f09661-beb8-84b9-8efb-b0379848397b>
2012-02-25 18:29:25 +0000

    - fixed a serious bug

--- trunk/BA-Robot-Driver/BaRobotDriverDll/BaRobotDriverDll/BaRobot.cpp
+++ trunk/BA-Robot-Driver/BaRobotDriverDll/BaRobotDriverDll/BaRobot.cpp
@@ -28,15 +28,26 @@ void BaRobot::SetMode(int tm)
 	if (tm == TransferMode::USB)
 		isValidTransferMode = true;
 	else
+	{
+		comPort = 0;
 		isValidTransferMode = false;
+	}
 }
 
 void BaRobot::SetComPort(int port)
 {
 	if (transferMode == TransferMode::COM)
 	{
-		comPort = port;
-		isValidTransferMode = true;
+		if ( (port > 0) && (port < 21) )
+		{
+		    comPort = port;
+		    isValidTransferMode = true;
+		}
+		else
+		{
+			comPort = 0;
+		}
+
 	}
 	else
 		comPort = 0;
@@ -44,7 +55,8 @@ void BaRobot::SetComPort(int port)
 
 char* BaRobot::SendString(char* message)
 {
-	if (isValidTransferMode)
+	// transfermode == Usb || (transfermode == com and port > 1 && port < 21 )
+	if (isValidTransferMode && strlen(message) < 200)
 	{
 		return communicate(message);
 	}
@@ -58,19 +70,22 @@ char* BaRobot::SendString(char* message)
 char* BaRobot::communicate(char* message)
 {
 	int notOkay = OpenComport(comPort - 1,9600);						// gives 1 if error and 0 if okay
-	char* newMessage = new char[sizeof(message) + 2];					// creation of new array for += '/n' 
+	char* newMessage = new char[strlen(message) + 2];
+    
 	strcpy(newMessage, message);
-	strcat(newMessage, "\n");
+	strcat(newMessage, "\n\0");
+
 	if (!notOkay)														// ugly to say if not not okay but the rs232 library give the returncodes that way
 	{
-		cprintf(comPort - 1, newMessage);								// writes to the serial line (comPort -1) because Com counts from 1
-		char* retBuffer = new char[100];								// return buffer for reading serial line (what has the device to say?)
+		cprintf(comPort - 1, newMessage);   							// writes to the serial line (comPort -1) because Com counts from 1
+		char* retBuffer = new char[1024];								// return buffer for reading serial line (what has the device to say?)
 		Sleep(100);														// wait for the device to send (from rs232 library where is said minimum 100 ms)
-		PollComport(9,(unsigned char*) retBuffer, sizeof(retBuffer));	// read the serial line
-		CloseComport(9);												// very important to close the serial connection
+		PollComport(comPort - 1,(unsigned char*) retBuffer, 1024);   	// read the serial line
+
+		CloseComport(comPort - 1);										// very important to close the serial connection
 		return retBuffer;												// returns what the device said
 	}
-	CloseComport(9);
+	CloseComport(comPort - 1);
 	return "Error opening the serial line...";
 }
 
