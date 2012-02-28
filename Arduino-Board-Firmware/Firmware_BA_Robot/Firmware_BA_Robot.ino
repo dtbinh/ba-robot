41ca4a9cda68e08e9fa061045446634a89768867
berdsen.home <berdsen.home@gmail.com>
2012-02-28 10:06:11 +0000

    - Firmware angepasst, Handling ausgelagert nach Functions.cpp

--- Arduino-Board-Firmware/Firmware_BA_Robot/Firmware_BA_Robot.ino
+++ Arduino-Board-Firmware/Firmware_BA_Robot/Firmware_BA_Robot.ino
@@ -1,13 +1,12 @@
 
-#include "ServoMotor.h"
 #include <EEPROM.h>
+#include "Functions.h"
 
-#include "StringArray.h"
+// #include "ServoMotor.h"
+// ServoMotor servo[8];
 
-ServoMotor servo[8];
 String inputString = "";
 boolean stringComplete = false;
-int currentCommandCount = 0;
 
 // Initializing Routine
 void setup() {      
@@ -16,6 +15,7 @@ void setup() {
   pinMode(13, OUTPUT);
 }
 
+// Main Loop
 void loop() {
 
   if (stringComplete) 
@@ -30,134 +30,4 @@ void loop() {
     }
   }
 }
-
-void serialEvent() 
-{
-  while (Serial.available()) 
-  {
-    char inChar = (char)Serial.read(); 
-    if (inChar == '\n' || inChar == '\r') {
-      stringComplete = true;
-      inputString += '\0';
-    }
-    else
-    { 
-      inputString += inChar;
-    } 
-  }
-}
-
-void handleSerialCommands()
-{
-  // EEPROM.write(0,0xFF);
-  // byte data = EEPROM.read(0);
-  // Serial.println(data,HEX);
-  // char** temp = SubstringCommandList(inputString);
-
-  /*
-  for (int i = 0; i < currentCommandCount; i++)
-   {
-   String tempString = temp[i];
-   Serial.print(tempString); 
-   Serial.print("<>"); 
-   } 
-   */
-  // Serial.print(inputString); 
-  // clear the string:
-  // StringArray array = GetCommandList(inputString);
-  // Serial.print(array.GetString(0));   
-  // Serial.print(array.GetString(0));   
-  if (inputString == "READ")
-  {
-    byte data = EEPROM.read(0);
-    inputString = inputString.substring(0,inputString.indexOf('\0')) + ": " + String(data,HEX) + "\0";
-    Serial.print(inputString); 
-    /*
-    String temp = String(data,HEX);
-    char p[10];
-    temp.toCharArray(p,10);
-    p[2] = '\0';
-    p[3] = 'H';
-    p[4] = 'U';    
-    temp += "\0";
-    String output = temp;
-    inputString += temp;
-      // Serial.print(inputString); 
-    // Serial.print(" ");
-    Serial.print(p);
-    */
-  } 
-  else if (inputString == "ON")
-  {
-    digitalWrite(13,HIGH);
-    Serial.print(inputString); 
-  }
-  else if (inputString == "OFF")
-  {
-    digitalWrite(13,LOW);
-    Serial.print(inputString); 
-  } 
-  else
-  {
-    Serial.print(inputString);
-  }
-  inputString = "";
-  stringComplete = false;
-}
-
-StringArray GetCommandList(String inputstr)
-{
-  StringArray retVal = StringArray();
-  
-  String temp = inputstr;
-
-  while (true)
-  {
-    if (temp.indexOf(';') == -1)
-      break;
-    else 
-    {
-      retVal.AddString(temp.substring(0,temp.indexOf(';')));
-      temp = temp.substring(temp.indexOf(';') + 1);
-    }
-  }
-  return retVal;
-}
-
-char** SubstringCommandList(String inputstr)
-{
-  String temp = inputstr;
-  int counter = 1;
-  while (true)
-  {
-    if (temp.indexOf(';') == -1)
-      break;
-    else 
-    {
-      counter++;
-      temp = temp.substring(temp.indexOf(';') + 1);
-    }
-  }
-  
-  currentCommandCount = counter;
-
-  char** retVal = new char*;
-  temp = inputstr;
-
-  for (int i = 0; i < counter -1; i++)
-  {
-    unsigned int length = (unsigned int)temp.indexOf(';');
-    char tempStorage[length];
-    String tempString = temp.substring(0,temp.indexOf(';'));
-    tempString.toCharArray(tempStorage, temp.indexOf(';'));
-    Serial.print(tempStorage);
-    retVal[i] = tempStorage;
-    temp = temp.substring(temp.indexOf(';'));
-  }
-  char tempStorage[temp.length()];
-  temp.toCharArray(tempStorage, temp.length());
-  retVal[counter] = tempStorage;
-  return retVal;
-}
-
 
---
+++ Arduino-Board-Firmware/Firmware_BA_Robot/Functions.cpp
@@ -0,0 +1,88 @@
+#include "Functions.h"
+
+void serialEvent() 
+{
+  while (Serial.available()) 
+  {
+    char inChar = (char)Serial.read(); 
+    if (inChar == '\n' || inChar == '\r') {
+      stringComplete = true;
+      inputString += '\0';
+    }
+    else
+    { 
+      inputString += inChar;
+    } 
+  }
+}
+
+void handleSerialCommands()
+{
+  StringArray commandList = GetCommandList(inputString);
+
+  if (commandList.GetString(0) == "READ")
+  {
+    /*
+    EEPROM.write(0,0xFF);
+    EEPROM.write(1,0xFE);
+    EEPROM.write(2,0xFD);
+    */
+    String temp = "";
+    if (commandList.GetElementCount() > 1)
+      temp = commandList.GetString(1);
+    else
+      temp = "0";
+      
+    char tempArray[temp.length() + 1];
+    temp.toCharArray(tempArray,temp.length() + 1);
+    tempArray[temp.length()] = '\0';
+    int test = atoi(tempArray);
+    byte data = EEPROM.read(test);
+    String outputstring = commandList.GetString(0) + ": [" + commandList.GetString(1) + "]: " + String(data,HEX) + "\0";
+
+    Serial.print(outputstring);    
+  } 
+  else if (commandList.GetString(0) == "ON")
+  {
+    digitalWrite(13,HIGH);
+    Serial.print(commandList.GetString(0)); 
+  }
+  else if (commandList.GetString(0) == "OFF")
+  {
+    digitalWrite(13,LOW);
+    Serial.print(commandList.GetString(0)); 
+  } 
+  else
+  {
+    for (int i = 0; i < commandList.GetElementCount(); i++)
+    {
+      Serial.print(commandList.GetString(i));
+      Serial.print("|");
+    }
+  }
+  inputString = "";
+  stringComplete = false;
+}
+
+StringArray GetCommandList(String inputstr)
+{
+  StringArray retVal;
+  
+  String temp = inputstr;
+
+  while (true)
+  {
+    if (temp.indexOf(';') < 0)
+    {
+      retVal.AddString(temp);  
+      break;
+    }
+    else 
+    {
+      retVal.AddString(temp.substring(0,temp.indexOf(';')));
+      temp = temp.substring(temp.indexOf(';') + 1);
+    }
+  }
+  return retVal;
+}
+
