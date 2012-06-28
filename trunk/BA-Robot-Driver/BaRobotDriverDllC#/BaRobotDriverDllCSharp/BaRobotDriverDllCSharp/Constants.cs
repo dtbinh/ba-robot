a8606e3e7e943fdd9d96198638a5cc11858d7de3
berdsen.home@gmail.com <berdsen.home@gmail.com@e0f09661-beb8-84b9-8efb-b0379848397b>
2012-06-28 09:10:25 +0000

    Ba-Robot-Driver: - BaRobotDriverDllC#:   - Ported C++ Dll to CSharp

---
+++ trunk/BA-Robot-Driver/BaRobotDriverDllC#/BaRobotDriverDllCSharp/BaRobotDriverDllCSharp/Constants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BaRobotDriverDllCSharp
+{
+    public class Constants
+    {
+        public static class TransferMode
+        {
+            public const int USB = 0;
+            public const int COM = 1;
+        }
+    }
+}
